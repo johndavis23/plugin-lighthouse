798298451d7cd110132ec7ca8b8eed8a0a75241e
John Davis <john_davis@discovery.com>
2020-09-15 13:52:09 -0400

    Some logging

--- aggregator.js
+++ aggregator.js
@@ -15,20 +15,24 @@ class Aggregator {
       this.log.debug(`stat ${path} was undefined, skipping`);
       return;
     }
+    this.log.debug(`JD pushing stats ${path} : ${value}`);
     this.statsHelpers.pushStats(this.stats, path, value);
   }
 
   addToAggregate(result) {
     forEach(result.categories, category => {
+      this.log.debug(`JD add to aggregate ${category.id} ${category.score}`);
       this._pushStats(['categories', category.id], category.score);
     });
 
     forEach(result.audits, audit => {
       switch (audit.scoreDisplayMode) {
         case 'numeric':
+          this.log.debug(`JD add to aggregate audits ${audit.id} ${audit.numericValue}`);
           this._pushStats(['audits', audit.id], audit.numericValue);
           break;
         case 'binary':
+          this.log.debug(`JD add to aggregate binary audits ${audit.id} ${audit.score}`);
           this._pushStats(['audits', audit.id], audit.score);
           break;
         default:
@@ -39,8 +43,10 @@ class Aggregator {
 
   summarize() {
     if (Object.keys(this.stats).length === 0) {
+      this.log.debug(`JD returning undefined due to stats length 0`);
       return undefined;
     }
+    this.log.debug(`JD Sum per object`);
     return this.summarizePerObject(this.stats);
   }
 
